434b7dac8d4e2eb60befbda7621890b6e208640a
Satria Junanda <satrialin34@gmail.com>
2026-05-25 14:11:03 +0800

    Update fcm_server.py
    backup

--- fcm_server.py
+++ fcm_server.py
@@ -15,7 +15,7 @@ from google.oauth2 import service_account
 import requests
 
 app = Flask(__name__)
-CORS(app, origins=["https://satria7.github.io"])
+CORS(app)
 
 PROJECT_ID = "klik-ngoerah"
 SERVICE_ACCOUNT_FILE = "serviceAccount.json"
